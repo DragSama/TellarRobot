e0dfc563aa7ebb2ee19974254382dce9c04ab0c7
DragSama <49992845+DragSama@users.noreply.github.com>
2020-10-09 16:48:22 +0530

    Create sample_config.py

---
+++ TellarBot/sample_config.py
@@ -0,0 +1,17 @@
+LOG_CHANNEL = "Username" # Self explantory
+BOTS_LIST = ["MYAMAZINGBOT1", "MYNOTSOAMAZINGBOT1"] # List of bots to check
+BOTS_CONFIG = {
+"MYAMAZINGBOT1": {
+        "username": "bot", # Username of bot
+        "avg_response_time": 5 # How much time can bot take to respond
+    },
+"MYNOTSOAMAZINGBOT1": {
+        "username": "bot",
+        "avg_response_time": 5,
+        "command": "/hello" # Command to test, Defaults to /start
+    }
+}
+
+API_ID = 12345 # Self explantory
+API_HASH = "1234abcd" # Self explantory
+STRING_SESSIONS = "SUPER-LONG-STRING" 
